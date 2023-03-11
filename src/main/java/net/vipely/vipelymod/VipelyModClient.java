367bc034a430ecebf7db8420e9ad6aa0f3f463a8
Nathan <theboi2017@gmail.com>
2023-03-11 16:04:41 -0500

    Push code to Github

---
+++ src/main/java/net/vipely/vipelymod/VipelyModClient.java
@@ -0,0 +1,12 @@
+package net.vipely.vipelymod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class VipelyModClient implements ClientModInitializer{
+
+	@Override
+	public void onInitializeClient() {
+		
+	}
+
+}
